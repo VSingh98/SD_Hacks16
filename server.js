80b14a3a0ce4e91e01543db4a8074edd386f915e
Archit Mishra <armishra@ucsd.edu>
2016-10-01 19:38:48 -0700

    nodejs port

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req, res){
+	res.sendFile(__dirname + '/index.html')
+});
+
+var prt = 8000;
+app.listen(prt);
+
+console.log("Running at Port " + prt);

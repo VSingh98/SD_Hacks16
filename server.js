4992471ac53c71f190fc07c9bbfdd52206ac3f59
Archit Mishra <armishra@ucsd.edu>
2016-10-01 20:25:12 -0700

    added parsing email

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/',function(req, res){
 	res.sendFile(__dirname + '/index.html')
+});
+
+app.post('/api/email', function(req, res){
+	console.log("Posted info " + req.body);
 });
 
 var prt = 8000;
